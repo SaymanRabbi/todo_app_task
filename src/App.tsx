b150b10b886cb4db6cc250c3ca81d3d795530490
sayman rabbi <mdrabbi.asm@gmail.com>
2024-02-06 17:25:04 +0600

    install package react hook form and zustand

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Container from './Components/Container/Container'
 
 function App() {
 
 
   return (
-  <h2 className=' text-teal-200'>Hello Word</h2>
+  <Container className=' py-10'>
+    <h1 className=' text-[40px] text-white'>Hello</h1>
+  </Container>
   )
 }
 
---
+++ src/Components/Container/TaskForm.tsx
@@ -0,0 +1,11 @@
+
+
+const TaskForm = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default TaskForm;
